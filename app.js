b703cea321e2f7305fca9310136a9f5ca44e7c36
Adam Robson <adamray312@gmail.com>
2022-07-20 12:52:50 -0700

    added eventlistener and other user input functionality

--- app.js
+++ app.js
@@ -10,5 +10,16 @@ container.style.backgroundColor = 'blue';
 
 // set event listeners 
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
+  
+const nameInput = document.getElementById('name-input');
+const updateButton = document.getElementById('update-button');
+const nameDiv = document.getElementById('name');
+
+updateButton.addEventListener('click', () => { });
+
+updateButton.addEventListener('click', () => {
+    const value = nameInput.value;
+    nameDiv.textContent = value;
+});
