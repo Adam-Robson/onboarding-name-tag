47182c81227925ca22e0470e084528b454db88c1
Adam Robson <adamray312@gmail.com>
2022-07-20 13:12:23 -0700

    tidied up the JS file

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 // import functions and grab DOM elements
-const nameElem = document.getElementById('name');
-nameElem.textContent = 'Benny';
-nameElem.style.color = 'green';
-
+const nameInput = document.getElementById('name-input');
+const updateButton = document.getElementById('update-button');
 const container = document.getElementById('name-tag');
 container.style.backgroundColor = 'blue';
+const greeting = document.getElementById('greeting');
+const nameDiv = document.getElementById('name');
+nameDiv.textContent = 'Enter name here';
+nameDiv.style.color = 'green';
 
 // let state
 
 // set event listeners 
+updateButton.addEventListener('click', () => { });
   // get user input
   // use user input to update state
-  // update DOM to reflect the new state
-  
-const nameInput = document.getElementById('name-input');
-const updateButton = document.getElementById('update-button');
-const nameDiv = document.getElementById('name');
-
-updateButton.addEventListener('click', () => { });
-
 updateButton.addEventListener('click', () => {
     const value = nameInput.value;
     nameDiv.textContent = value;
 });
+  // update DOM to reflect the new state
+  
+
+
+
+
+
+
